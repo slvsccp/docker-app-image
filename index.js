3a381ccecbeb4a40812e789475e9df4a4102d2fb
Matheus Silva <teus_26@outlook.com>
2024-07-11 18:16:58 -0300

    image update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Docker!');
+  res.send('Hello Docker! Update!');
 });
 
 app.listen(port, () => {
-  console.log(`App rodando na porta: ${port}`);
+  console.log(`App run on the port: ${port}`);
 });
